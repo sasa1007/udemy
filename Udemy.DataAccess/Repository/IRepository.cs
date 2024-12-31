8503d0299910e554b867b48bed1860780344eedf
backend2 <sasam09@gmail.com>
2024-12-31 12:14:02 +0100

    70

---
+++ Udemy.DataAccess/Repository/IRepository.cs
@@ -0,0 +1,12 @@
+using System.Linq.Expressions;
+
+namespace udemy.Udemy.DataAccess.Repository;
+
+internal interface IRepository<T> where T : class
+{
+    IEnumerable<T> GetAll();
+    T Get(Expression<Func<T, bool>> filter);
+    void Create(T entity);
+    void Delete(T entity);
+    void DeleteRange(IEnumerable<T> entity);
+}
